7ea69ab31a69dc9bec57ffaa4b5e96f18174c289
Balthazar <balthazar.muhlstein@etu.univ-lyon1.fr>
2024-08-29 22:08:24 +0200

    Ajout Analyse eco-conception + disparition header click

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,24 +2,32 @@ import { Divider, Grid, Typography } from "@mui/material"
 import { FaGit, FaGitAlt } from "react-icons/fa"
 import "../styles/Footer.css"
 import { Stack } from "@mui/system"
+import { WebsiteCarbonBadge } from "react-websitecarbon-badge"
 
 const Footer: React.FC = () => {
     return (
         <section>
             <Grid container direction={"row"} justifyContent="center"
-                padding={"1%"}
+                padding={"0.2%"}
                 columnSpacing={2}
-                bgcolor={"white"}>
+                bgcolor={"white"}
+                alignItems={"center"} >
                 <Grid item>
                     <Typography color={"text.primary"}>Fait avec React</Typography>
                 </Grid>
-                <Grid item><Divider orientation="vertical"></Divider></Grid>
+                <Grid style={{alignSelf:"stretch"}} item><Divider orientation="vertical" ></Divider></Grid>
                 <Grid item>
 
                     <Typography color={"text.primary"}><a target="_blank" href="https://github.com/balthaLeSanglier/PortfolioReact"><Stack spacing={3} direction={"row"} justifyContent={"center"} alignItems={"center"}>Accès au GIT<FaGitAlt />                    </Stack>
                     </a></Typography>
                 </Grid>
+                <Grid style={{alignSelf:"stretch"}} item><Divider orientation="vertical" ></Divider></Grid>
+
+                <Grid item>
+                    <WebsiteCarbonBadge co2=" 0.27" lang="fr" percentage="70"></WebsiteCarbonBadge>
+                </Grid>
             </Grid>
+            
         </section>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,18 +7,42 @@ import { Typography, useMediaQuery } from '@mui/material';
 const Header: React.FC = () => {
   const matches = useMediaQuery('(min-width:600px)');
   const [small, setSmall] = useState<boolean>(false)
+  const [menuClick, setMenuClick] = useState<boolean>(false)
   useEffect(() => {
-
     window.addEventListener("scroll", () => {
       var isHidden = window.scrollY > 200
-      setSmall(isHidden)
+      if(menuClick) {
+        setSmall(false)
+      }
+      if(!menuClick) {
+        setSmall(isHidden)
+
+      }
+      console.log("smallScroll "+small)
+
     }
     );
   }, [])
+
+  const handleMenuClick = () => {
+    setMenuClick(true)
+    setSmall(false)
+    console.log("smallClick "+small)
+
+    setTimeout(() => {
+      setSmall(false)
+
+      setMenuClick(false)
+      console.log("menuClick "+menuClick);
+      console.log("menuSmall "+small)
+    }, 1000);
+    console.log("smallClick "+small)
+
+  }
   if (!matches) {
     return (
       <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} right>
-        <a href="#Portrait">A propos</a>
+        <a href="#Portrait" onClick={() => setMenuClick(true)}>A propos</a>
         <a href="#Techs">Technologies</a>
         <a href="#Projects">Projets</a>
         <a href="#ComptencyTable">Tableau de compétence</a>
@@ -61,31 +85,31 @@ const Header: React.FC = () => {
             </li>
           </ul>
         </nav>
-        <nav className={`floatingHeader ${small ? 'appears' : 'hiddenMenu'}`}>
+        <nav className={`floatingHeader ${small && !menuClick ? 'appears' : 'hiddenMenu'}`}>
           <ul>
             <li>
-              <a href="#Portrait">
+              <a href="#Portrait"  onClick={() => handleMenuClick()}>
                 <Typography variant="header">A propos</Typography>
               </a>
             </li>
             <li>
               <a href="#Techs">
-                <Typography variant="header">Technologies</Typography>
+                <Typography  onClick={() => handleMenuClick()} variant="header">Technologies</Typography>
               </a>
             </li>
             <li>
               <a href="#Projects">
-                <Typography variant="header">Projets</Typography>
+                <Typography  onClick={() => handleMenuClick()} variant="header">Projets</Typography>
               </a>
             </li>
             <li>
               <a href="#ComptencyTable">
-                <Typography variant="header">Tableau de compétence</Typography>
+                <Typography  onClick={() => handleMenuClick()} variant="header">Tableau de compétence</Typography>
               </a>
             </li>
             <li>
               <a href="#Contact">
-                <Typography variant="header">Contact</Typography>
+                <Typography onClick={() => handleMenuClick()} variant="header">Contact</Typography>
               </a>
             </li>
           </ul>
